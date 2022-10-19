83b87fa4cfa4519459c6d87fe72e8f92a40c297d
Reji <2111896@cognizant.com>
2022-10-19 23:27:05 +0530

    useReducer added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@
 import './App.css';
 import Layout from './context/Layout';
 import MainConten from './context/MainConten';
+import UserList from './Hooks/UserList';
 import UserInfo from './user/UserInfo';
 
 function App() {
   return (
-    <Layout>
-    <MainConten />
-
-
-    </Layout>
+    <UserList />
     
   );
 }
